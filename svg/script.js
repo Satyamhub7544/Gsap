196e4d09675c74e7ab8dfc74b6f60cfa13d43a9c
satyam singh <satyambgs75@gmail.com>
2026-07-24 19:50:49 +0530

    main

--- svg/script.js
+++ svg/script.js
@@ -1,1 +1,20 @@
+let pathValue = `M 10 80 Q 500 80 1000 80`;
+let finalValue = `M 10 80 Q 500 80 1000 80`;
 
+let object = document.querySelector(".box");
+
+object.addEventListener("mousemove", function (dets) {
+  pathValue = `M 10 80 Q 500 ${dets.y} 1000 80`;
+  gsap.to("svg path", {
+    attr: { d: pathValue },
+    ease: "power3.out",
+  });
+});
+
+object.addEventListener("mouseleave", function () {
+  gsap.to("svg path", {
+    attr: { d: finalValue },
+      duration:1,
+     ease: "elastic.out(1,0.2)",
+  });
+});
